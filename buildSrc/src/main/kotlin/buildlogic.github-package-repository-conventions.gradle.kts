26882e7119e9ce572741392dbc1fc18b4e9d90a0
René Lebherz <me@renelebherz.com>
2024-08-19 12:08:38 +0200

    fix: package repository access

--- buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
@@ -7,8 +7,8 @@ repositories {
         name = "GitHubPackages"
         url = uri("https://maven.pkg.github.com/bitfist/particle")
         credentials {
-            username = project.property("GITHUB_ACTOR") as String? ?: System.getenv("GITHUB_ACTOR")
-            password = project.property("GITHUB_TOKEN") as String? ?: System.getenv("GITHUB_TOKEN")
+            username = project.findProperty("GPR_USR") as String? ?: System.getenv("GPR_USR")
+            password = project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")
         }
     }
 }
