e07839573bcac1739c2c1148924dce287e33645c
René Lebherz <me@renelebherz.com>
2024-08-21 18:59:28 +0200

    chore: throw exception when build variables are missing

--- buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
@@ -7,8 +7,8 @@ repositories {
         name = "GitHubPackages"
         url = uri("https://maven.pkg.github.com/bitfist/particle")
         credentials {
-            username = project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")
-            password = project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")
+            username = (project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")) ?: throw IllegalStateException("GitHub Package Registry username missing")
+            password = (project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")) ?: throw IllegalStateException("GitHub Package Registry token missing")
         }
     }
 }
