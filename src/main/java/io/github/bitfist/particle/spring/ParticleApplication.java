586d3004835d9fea65c40e70d00ba24fb916a5bd
René Lebherz <me@renelebherz.com>
2024-08-21 21:49:03 +0200

    fix: Icon will only be displayed when the application context is created on the EDT

--- src/main/java/io/github/bitfist/particle/spring/ParticleApplication.java
+++ src/main/java/io/github/bitfist/particle/spring/ParticleApplication.java
@@ -6,19 +6,24 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.springframework.boot.SpringApplication;
 
+import javax.swing.SwingUtilities;
+import java.lang.reflect.InvocationTargetException;
+
 public class ParticleApplication {
 
-    public static void start(Class<?> applicationClass, String[] args) {
-        System.setProperty("java.awt.headless", "false");
-        var applicationContext = SpringApplication.run(applicationClass, args);
+    public static void run(Class<?> applicationClass, String[] args) throws InterruptedException, InvocationTargetException {
+        SwingUtilities.invokeAndWait(() -> {
+            System.setProperty("java.awt.headless", "false");
+            var applicationContext = SpringApplication.run(applicationClass, args);
 
-        var particleWindow = applicationContext.getBean(ParticleWindow.class);
-        var urlProvider = applicationContext.getBean(ParticleWindowURLProvider.class);
-        var display = applicationContext.getBean(Display.class);
-        var shell = applicationContext.getBean(Shell.class);
+            var display = applicationContext.getBean(Display.class);
+            var particleWindow = applicationContext.getBean(ParticleWindow.class);
+            var urlProvider = applicationContext.getBean(ParticleWindowURLProvider.class);
+            var shell = applicationContext.getBean(Shell.class);
 
-        particleWindow.open(urlProvider.getURL());
-        startEventLoop(display, shell);
+            particleWindow.open(urlProvider.getURL());
+            startEventLoop(display, shell);
+        });
     }
 
     private static void startEventLoop(Display display, Shell shell) {
