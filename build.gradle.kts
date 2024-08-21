af30b5e74437e136df9caa4a52adc557dccfc71f
René Lebherz <me@renelebherz.com>
2024-08-21 19:11:30 +0200

    chore: improve compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,20 @@ plugins {
 	id("buildlogic.github-package-repository-conventions")
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 dependencies {
 	api("io.github.bitfist:particle:0.1.3")
 
-	implementation(platform("org.springframework.boot:spring-boot-dependencies:3.3.2"))
+	val springBootVersion = "3.3.2"
+	implementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:$springBootVersion")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("com.fasterxml.jackson.core:jackson-databind")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
