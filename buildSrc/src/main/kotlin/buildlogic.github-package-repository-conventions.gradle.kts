78aae588dd421d27987385bf7bd167a33e038a47
René Lebherz <me@renelebherz.com>
2024-09-24 09:36:42 +0200

    chore: remove debug output

--- buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
@@ -9,7 +9,6 @@ repositories {
         credentials {
             username = (project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")) ?: throw IllegalStateException("GitHub Package Registry username missing")
             password = (project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")) ?: throw IllegalStateException("GitHub Package Registry token missing")
-            println("using auth $username=$password")
         }
     }
 }
