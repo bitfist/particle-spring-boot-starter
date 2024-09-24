c6051cc3818efc45a924c0d8503b378a16de4d83
René Lebherz <me@renelebherz.com>
2024-09-24 09:34:36 +0200

    chore: add temporary auth to output for debug purposes

--- buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.github-package-repository-conventions.gradle.kts
@@ -9,6 +9,7 @@ repositories {
         credentials {
             username = (project.findProperty("GPR_USER") as String? ?: System.getenv("GITHUB_ACTOR")) ?: throw IllegalStateException("GitHub Package Registry username missing")
             password = (project.findProperty("GPR_KEY") as String? ?: System.getenv("GPR_KEY")) ?: throw IllegalStateException("GitHub Package Registry token missing")
+            println("using auth $username=$password")
         }
     }
 }
